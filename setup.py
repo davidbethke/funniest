d2d2c0ab8c209b88028d48b95b8f101234698fe3
david bethke <david.bethke@gmail.com>
2019-01-15 12:00:09 -0600

    added sdist file python setup.py sdist

---
+++ setup.py
@@ -0,0 +1,4 @@
+from setuptools import setup
+
+setup(name='funniest', version='0.1', description='funny', url='http://github.com/davidbethke/funniest', author='jk', author_email='jk@gmail.com', license='MIT', packages=['funniest'], zip_safe=False)
+
